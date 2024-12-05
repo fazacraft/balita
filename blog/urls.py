442eea64ac87e38c97137b7fdca1504d26fda6d2
Fazliddin <phazliddinphozilov@gmail.com>
2024-12-05 17:50:19 +0500

    added new url to blog.urls

--- blog/urls.py
+++ blog/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('blog/<int:pk>/', blog_detail_view),
     path('buyerda404/', blog_error_view),
     path('searchbek/', search_view),
-    path('www/', search_view)
+    path('www/', search_view),
+    21312
 ]
